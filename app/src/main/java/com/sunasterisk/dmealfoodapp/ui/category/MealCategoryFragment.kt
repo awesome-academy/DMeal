cad08111526c08f30fdd4dfe83dd854851123b8a
datvt-2504 <vu.tien.dat-b@sun-asterisk.com>
2021-05-13 10:15:03 +0700

    task#35870

---
+++ app/src/main/java/com/sunasterisk/dmealfoodapp/ui/category/MealCategoryFragment.kt
@@ -0,0 +1,17 @@
+package com.sunasterisk.dmealfoodapp.ui.category
+
+import com.sunasterisk.dmealfoodapp.R
+import com.sunasterisk.dmealfoodapp.base.BaseFragment
+
+class MealCategoryFragment : BaseFragment(R.layout.fragment_meal_category) {
+
+    override fun onCreatedView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initActions() {
+    }
+
+}
--- app/src/main/java/com/sunasterisk/dmealfoodapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/sunasterisk/dmealfoodapp/ui/main/MainActivity.kt
@@ -2,12 +2,29 @@ package com.sunasterisk.dmealfoodapp.ui.main
 
 import android.content.Context
 import android.content.Intent
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sunasterisk.dmealfoodapp.R
 import com.sunasterisk.dmealfoodapp.base.BaseActivity
+import com.sunasterisk.dmealfoodapp.ui.category.MealCategoryFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
+
+    private val onBottomNavigationView =
+        BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.menuCategory -> openFragment(MealCategoryFragment())
+            }
+            true
+        }
+
     override fun onCreatedView() {
+        bottomNavigationView.apply {
+            setOnNavigationItemSelectedListener(onBottomNavigationView)
+            selectedItemId = R.id.menuCategory
+        }
     }
 
     companion object {

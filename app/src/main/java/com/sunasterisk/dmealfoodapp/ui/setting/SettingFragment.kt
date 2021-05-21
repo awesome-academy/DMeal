6c3686dbf7124add7c08a66913ead6d6e2e1f38e
datvt-2504 <vu.tien.dat-b@sun-asterisk.com>
2021-05-21 15:30:02 +0700

    task35902

---
+++ app/src/main/java/com/sunasterisk/dmealfoodapp/ui/setting/SettingFragment.kt
@@ -0,0 +1,25 @@
+package com.sunasterisk.dmealfoodapp.ui.setting
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.sunasterisk.dmealfoodapp.base.BaseFragment
+import com.sunasterisk.dmealfoodapp.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentSettingBinding =
+        FragmentSettingBinding::inflate
+
+    override fun onCreatedView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initActions() {
+
+    }
+
+}
